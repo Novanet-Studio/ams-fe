62a4256484d0b3d8f26b0b4eecd47de321c33368
David Arenas <davejs136@gmail.com>
2023-11-15 20:28:35 -0400

    refactor: heading

---
+++ src/lib/components/header/Header.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import AmsLogo from "./AmsLogo.svelte";
+	import BurgerMenu from "./BurgerMenu.svelte";
+</script>
+
+<header class="flex border border-red-500 items-center justify-between">
+  <AmsLogo />
+  <BurgerMenu />
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,11 @@
 <script lang="ts">
   import 'virtual:uno.css';
   import '@unocss/reset/tailwind.css';
-	import BurgerMenu from '$lib/components/BurgerMenu.svelte';
-	import AmsLogo from '$lib/components/AmsLogo.svelte';
+	import Header from '$lib/components/header/Header.svelte';
 </script>
 
 <div class="font-primary p-6">
-  <header class="flex border border-red-500 items-center justify-between">
-    <AmsLogo />
-    <BurgerMenu />
-  </header>
+  <Header />
   <main>
     <slot />
   </main>
