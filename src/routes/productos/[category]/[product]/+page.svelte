96bbd10a449c0817d7f6f6b9b1fe4d55eedc5245
David Arenas <davejs136@gmail.com>
2023-12-14 11:26:05 -0400

    feat: add mount animation for products

--- src/routes/productos/[category]/[product]/+page.svelte
+++ src/routes/productos/[category]/[product]/+page.svelte
@@ -2,6 +2,8 @@
 	import { page } from '$app/stores';
 	import { productsImages } from '$lib/images';
 	import { getImage } from '$lib/utils';
+	import { stagger, timeline } from 'motion';
+	import { onMount } from 'svelte';
 
 	interface Data {
 		title: string;
@@ -20,17 +22,77 @@
 		(category) => category.name.toLowerCase() === categoryName
 	);
 	$: data.content.products.forEach((product) => {
-		const result = product.items.filter((item) => item.category === productName);
+		const result = product.items.filter((item: any) => item.category === productName);
 
 		if (result.length) {
 			products = result;
 		}
 	});
 
-	// const getImage = (path: string) => import.meta.resolve(`../../../../lib/${path}`);
+	onMount(() => {
+		timeline(
+			[
+				[
+					'#products',
+					{
+						opacity: [0, 1],
+						background: ['#fff', '#003B49']
+					},
+					{ duration: 0.4, easing: 'ease-out' }
+				],
+				[
+					'#products #image',
+					{ opacity: [0, 1], filter: ['blur(2px)', 'blur(0)'] },
+					{ duration: 0.4, easing: 'ease-out' }
+				],
+				[
+					'#products #middle',
+					{ opacity: [0, 1], x: [-10, 0] },
+					{ duration: 0.5, easing: 'ease-out', delay: 0.2 }
+				],
+				[
+					'#products #middle #name',
+					{ opacity: [0, 1], x: [-10, 0] },
+					{ duration: 0.5, easing: 'ease-out', delay: 0.2 }
+				],
+				[
+					'#products #top',
+					{
+						opacity: [0, 1],
+						clipPath: [
+							'polygon(0% 100%, 100% 100%, 90% 70%)',
+							'polygon(0% 100%, 100% 100%, 73.49% 50.75%)'
+						]
+					},
+					{ duration: 0.4, easing: 'ease-out' }
+				],
+				[
+					'#products #bottom',
+					{
+						opacity: [0, 1],
+						clipPath: ['polygon(40% 70%, 0 0, 100% 0)', 'polygon(32% 60%, 0 0, 100% 0)']
+					},
+					{ duration: 0.4, easing: 'ease-out' }
+				],
+				[
+					'#products ul h3',
+					{ opacity: [0, 1], y: [-10, 0] },
+					{ duration: 0.5, easing: 'ease-out' }
+				],
+				[
+					'#products ul li',
+					{ opacity: [0, 1], y: [10, 0] },
+					{ duration: 0.5, easing: 'ease-out', delay: stagger(0.1) }
+				]
+			],
+			{
+				duration: 2.5
+			}
+		);
+	});
 </script>
 
-<section class="h-screen bg-#003B49 flex flex-col items-center pt-14">
+<section id="products" class="h-screen bg-#003B49 flex flex-col items-center pt-14">
 	<div class="w-full">
 		<div
 			id="top"
@@ -39,7 +101,7 @@
 		<div class="w-full h-36 relative overflow-hidden">
 			<div
 				id="middle"
-				class="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-#93B7BB/50 gap-2"
+				class="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-#93B7BB/50 gap-2 z-2"
 			>
 				<h4 id="name" class="text-#003B49 font-bold text-lg">{category?.name}</h4>
 			</div>
