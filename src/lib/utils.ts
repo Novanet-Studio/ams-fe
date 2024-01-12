fa30b2a210925b6365ce1c22955b1c1f52d738d3
David Arenas <davejs136@gmail.com>
2024-01-11 22:27:03 -0400

    feat: get product by API

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,6 @@ export const getImage = (name: string) => name.replace('.png', '');
 
 export const getImageUrl = (path: string | undefined) =>
 	isDev ? `http://localhost:1337${path}` : path;
+
+export const capitalizeFirstLetter = (string: string) =>
+	string.charAt(0).toUpperCase() + string.slice(1);
--- src/routes/productos/[category]/+page.ts
+++ src/routes/productos/[category]/+page.ts
@@ -1,10 +1,7 @@
+import { capitalizeFirstLetter } from '$lib/utils';
 import type { SubcategoriesVariables } from './$houdini';
 
 export const ssr = false;
-
-function capitalizeFirstLetter(string) {
-	return string.charAt(0).toUpperCase() + string.slice(1);
-}
 
 export const _SubcategoriesVariables: SubcategoriesVariables = ({ params }) => ({
 	name: capitalizeFirstLetter(params.category)
