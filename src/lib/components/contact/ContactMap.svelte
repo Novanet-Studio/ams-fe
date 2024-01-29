77e74c3d7297cddc39b4502fdf0fe84b3475c9ff
David Arenas <davejs136@gmail.com>
2024-01-29 12:03:21 -0400

    feat: add example contact map

--- src/lib/components/contact/ContactMap.svelte
+++ src/lib/components/contact/ContactMap.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { isDesktop } from '$lib/store';
+	import { elementColors, isDesktop } from '$lib/store';
 	import { timeline } from 'motion';
 	import { onMount } from 'svelte';
 	import { fade } from 'svelte/transition';
@@ -46,11 +46,22 @@
 				duration: 2
 			}
 		);
+		$elementColors.burger = 'dark';
 	});
 </script>
 
 <section bind:this={section} class="w-full h-screen snap-start relative" transition:fade>
-	<div id="map" class="bg-#003B49 h-84vh lg:h-81.4vh"></div>
+	<div id="map" class="bg-#003B49 h-84vh lg:h-81.4vh">
+		<iframe
+			class="w-full h-full"
+			title="Mapa de ubicación de Avila Multisport"
+			src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d31385.916957960602!2d-66.82160975000001!3d10.48176725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2sve!4v1706543997428!5m2!1ses!2sve"
+			style="border:0;"
+			allowfullscreen={false}
+			loading="lazy"
+			referrerpolicy="no-referrer-when-downgrade"
+		></iframe>
+	</div>
 	<div
 		id="mapFill"
 		class="absolute top-0 right-0 bottom-0 [clip-path:polygon(0%_0%,_100%_0%,_100%_88.75%,_0%_83.06%)] bg-#ACC37E w-full -z-1 lg:h-97.2vh"
