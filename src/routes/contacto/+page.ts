a0e4caae96a5f08af827a36ff0f4536b6254d223
David Arenas <davejs136@gmail.com>
2024-02-01 15:25:47 -0400

    fix(contact): remove unused +page.ts request

--- src/routes/contacto/+page.ts
+++ src/routes/contacto/+page.ts
@@ -1,19 +1,2 @@
 export const ssr = false;
 export const prerender = true;
-
-/** @type {import('./$types').PageLoad} */
-export async function load({ fetch }) {
-	const res = await fetch('/api/info?key=about');
-
-	if (res.ok) {
-		const content = await res.json();
-
-		return {
-			content
-		};
-	}
-
-	return {
-		content: []
-	};
-}
