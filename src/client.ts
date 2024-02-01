b348134db6569765ee65d24de0acb534cab92287
David Arenas <davejs136@gmail.com>
2024-02-01 15:28:45 -0400

    refactor(houdini): set production backend url

--- src/client.ts
+++ src/client.ts
@@ -1,15 +1,15 @@
 import { HoudiniClient } from '$houdini';
 
 export default new HoudiniClient({
-    url: 'http://localhost:1337/graphql'
+	url: 'https://avilamultisport-be-a6e286d55805.herokuapp.com/graphql'
 
-    // uncomment this to configure the network call (for things like authentication)
-    // for more information, please visit here: https://www.houdinigraphql.com/guides/authentication
-    // fetchParams({ session }) {
-    //     return {
-    //         headers: {
-    //             Authentication: `Bearer ${session.token}`,
-    //         }
-    //     }
-    // }
-})
+	// uncomment this to configure the network call (for things like authentication)
+	// for more information, please visit here: https://www.houdinigraphql.com/guides/authentication
+	// fetchParams({ session }) {
+	//     return {
+	//         headers: {
+	//             Authentication: `Bearer ${session.token}`,
+	//         }
+	//     }
+	// }
+});
