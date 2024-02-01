5ebdb28e39965ae33848477316924a55386276c6
David Arenas <davejs136@gmail.com>
2024-02-01 16:01:17 -0400

    fix(products): set prerender as `true`

--- src/routes/productos/[category]/+page.ts
+++ src/routes/productos/[category]/+page.ts
@@ -2,6 +2,7 @@ import { capitalizeFirstLetter } from '$lib/utils';
 import type { SubcategoriesVariables } from './$houdini';
 
 export const ssr = false;
+export const prerender = true;
 
 export const _SubcategoriesVariables: SubcategoriesVariables = ({ params }) => ({
 	name: capitalizeFirstLetter(params.category)
--- src/routes/productos/[category]/[product]/+page.ts
+++ src/routes/productos/[category]/[product]/+page.ts
@@ -2,6 +2,7 @@ import { capitalizeFirstLetter } from '$lib/utils';
 import type { SubcategoryProductsVariables } from './$houdini';
 
 export const ssr = false;
+export const prerender = true;
 
 export const _SubcategoryProductsVariables: SubcategoryProductsVariables = ({ params }) => ({
 	name: capitalizeFirstLetter(params.product)
