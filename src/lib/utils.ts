9b779352ff1c225d3a6e687b00f00c8d85c5a381
David Arenas <davejs136@gmail.com>
2024-02-14 13:39:20 -0400

    refactor(utils): getImageUrl using `isLocalAssets` as validator

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,11 @@
-export const isDev = import.meta.env.DEV;
+import { env } from '$env/dynamic/public';
+
+export const isLocalAssets = env.PUBLIC_LOCAL_ASSETS || false;
 
 export const getImage = (name: string) => name.replace('.png', '');
 
 export const getImageUrl = (path: string | undefined) =>
-	isDev ? `http://localhost:1337${path}` : path;
+	isLocalAssets ? `http://localhost:1337${path}` : path;
 
 export const capitalizeFirstLetter = (string: string) =>
 	string.charAt(0).toUpperCase() + string.slice(1);
