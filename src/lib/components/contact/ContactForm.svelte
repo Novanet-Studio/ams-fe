a41eb792fb7f30423441a23e4f0c66e0d1da8da2
Eliezer A Meza <150059983+EliezerMezaDev@users.noreply.github.com>
2025-04-24 21:16:36 -0400

    feat: contact form with vanilla js

--- src/lib/components/contact/ContactForm.svelte
+++ src/lib/components/contact/ContactForm.svelte
@@ -3,6 +3,46 @@
 	import { isDesktop } from '$lib/store';
 	import { animate, stagger, timeline } from 'motion';
 
+	let sending = false;
+	let error = '';
+	let success = '';
+	const EMAIL_TARGET = 'eliezermeza.dev@gmail.com';
+
+	function sendForm(_event: Event) {
+		_event.preventDefault();
+		error = '';
+
+		try {
+			const formData = new FormData(_event.target as HTMLFormElement);
+			const formObject = Object.fromEntries(formData);
+
+			const emailBody = `Nombre: ${formObject.name}
+								Apellido: ${formObject.lastname}
+								Teléfono: ${formObject.phone}
+								Email: ${formObject.email}
+
+								Mensaje:
+								${formObject.message}
+			`;
+
+			console.log(`emailBody ${emailBody}`);
+
+			const subject = encodeURIComponent('CONTACTO DESDE FORMULARIO - AVILA MULTISPORT');
+			const body = encodeURIComponent(emailBody);
+
+			const gmailLink = `https://mail.google.com/mail/?view=cm&fs=1&to=${EMAIL_TARGET}&su=${subject}&body=${body}`;
+			window.open(gmailLink, '_blank');
+		} catch (e) {
+			error = 'Error al procesar el formulario';
+		} finally {
+			setTimeout(() => {
+				(_event.target as HTMLFormElement).reset();
+
+				console.log('Clean form');
+			}, 1000);
+		}
+	}
+
 	function enterAnimation() {
 		timeline(
 			[
@@ -83,24 +123,26 @@
 >
 	<div class="p-8 pt-24 bg-#003B49 h-94vh md:(pt-32 p-12) lg:pl-16">
 		<h3 class="mb-4 text-2xl text-#E3D268 md:text-4xl">Contáctanos</h3>
-		<form
-			class="flex flex-col gap-4 md:(gap-4 mt-8)"
-			name="contact-form"
-			method="POST"
-			netlify-honeypot="bot-field"
-			data-netlify="true"
-		>
+		<form class="flex flex-col gap-4 md:(gap-4 mt-8)" name="contact-form" on:submit={sendForm}>
 			<input type="hidden" name="form-name" value="contact-form" />
 			<div class="flex flex-col gap-4 lg:(flex-row)">
-				<input class="w-full" type="text" name="name" placeholder="Nombre" />
-				<input class="w-full" type="text" name="phone" placeholder="Teléfono" />
+				<input required class="w-full" type="text" name="name" placeholder="Nombre" />
+				<input required class="w-full" type="text" name="phone" placeholder="Teléfono" />
 			</div>
 			<div class="flex flex-col gap-4 lg:(flex-row)">
-				<input class="w-full" type="text" name="lastname" placeholder="Apellido" />
-				<input class="w-full" type="email" name="email" placeholder="Email" />
+				<input required class="w-full" type="text" name="lastname" placeholder="Apellido" />
+				<input required class="w-full" type="email" name="email" placeholder="Email" />
 			</div>
-			<textarea class="pt-3 pl-3" name="message" id="" rows="4" placeholder="Mensaje"></textarea>
-			<button class="self-start bg-#ACC37E py-2 px-12 rounded-full">Enviar</button>
+			<textarea required class="pt-3 pl-3" name="message" rows="4" placeholder="Mensaje"></textarea>
+			{#if error}
+				<p class="text-red-500">{error}</p>
+			{/if}
+			{#if success}
+				<p class="text-green-500">{success}</p>
+			{/if}
+			<button class="self-start bg-#ACC37E py-2 px-12 rounded-full" disabled={sending}>
+				{sending ? 'Enviando...' : 'Enviar'}
+			</button>
 		</form>
 	</div>
 </section>
