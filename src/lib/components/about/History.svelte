8d4a27da36f8ac35e6004142b19b7920be99395c
Fredy Polania <fpolania@gmail.com>
2025-07-29 12:40:39 -0400

    remove:onExit annimation for main About us section

--- src/lib/components/about/History.svelte
+++ src/lib/components/about/History.svelte
@@ -43,14 +43,6 @@
 			]
 		]);
 	}
-
-	function exitAnimation() {
-		animate(
-			'#history',
-			{ opacity: [1, 0] },
-			{ duration: 0.5, delay: 0.5, easing: [0.17, 0.55, 0.55, 1] }
-		);
-	}
 </script>
 
 <section
@@ -58,7 +50,6 @@
 	class="w-full h-screen snap-start relative"
 	use:inView={{ bottom: 100, top: 100 }}
 	on:enter={enterAnimation}
-	on:exit={exitAnimation}
 >
 	<div class="p-8 pt-24 bg-#003B49 h-94vh md:(pt-46 p-12) lg:pl-16">
 		<h3 class="mb-4 text-2xl text-#E3D268 md:text-4xl">{topic.title}</h3>
